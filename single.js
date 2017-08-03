d395437463943e2ca722bfc0ceb95229dbe7253a
Sergey Linev <S.Linev@gsi.de>
2017-08-03 12:32:42 +0200

    Reproduce error with TH1 stack
    Now fixed in JSROOT

--- single.js
+++ single.js
@@ -6,7 +6,26 @@ var fs = require("fs");
 var filename =  "http://jsroot.gsi.de/files/histpainter6.root",
     itemname = "draw_hstack";
 
+function MakeTest(file, item, callback) {
+   file.ReadObject(item, function(obj) {
+      jsroot.MakeSVG( { object: obj, width: 1200, height: 800 }, function(svg) {
+         fs.writeFileSync(item + ".svg", svg);
+         console.log('create ' + item + '.svg file size = ' + svg.length);
+         if (callback) callback();
+      });
+   });
+}
+
 jsroot.OpenFile(filename, function(file) {
+   MakeTest(file,"draw_hstack", function() {
+      MakeTest(file,"draw_nostackb", function() {
+         MakeTest(file,"draw_hstack", function() {
+         });
+      });
+   });
+   return;
+   
+   
    file.ReadObject(itemname, function(obj) {
       // var subpad = obj.fPrimitives.arr[2];
       var subpad = obj;
