31081efd39f8e4115b45c0dff90fd3e2c7c388c6
Sergey Linev <S.Linev@gsi.de>
2017-09-18 13:38:17 +0200

    Changes in text drawing
    Try to optimize and correctly adjust position

--- other/single.js
+++ other/single.js
@@ -4,7 +4,7 @@ var jsroot = require("jsroot");
 var fs = require("fs");
 
 var filename =  "http://jsroot.gsi.de/files/histpainter6.root",
-    itemname = "draw_hstack";
+    itemname = "draw_contlst1";
 
 function MakeTest(file, item, callback) {
    file.ReadObject(item, function(obj) {
@@ -17,19 +17,19 @@ function MakeTest(file, item, callback) {
 }
 
 jsroot.OpenFile(filename, function(file) {
-   MakeTest(file,"draw_hstack", function() {
+/*   MakeTest(file,"draw_hstack", function() {
       MakeTest(file,"draw_nostackb", function() {
          MakeTest(file,"draw_hstack", function() {
          });
       });
    });
    return;
-   
+*/   
    
    file.ReadObject(itemname, function(obj) {
       // var subpad = obj.fPrimitives.arr[2];
-      var subpad = obj;
-      jsroot.MakeSVG( { object: subpad, width: 1200, height: 800 }, function(svg) {
+      // var subpad = obj;
+      jsroot.MakeSVG( { object: obj, width: 1200, height: 800 }, function(svg) {
          fs.writeFileSync("single.svg", svg);
          console.log('create single.svg file size', svg.length);
       });
