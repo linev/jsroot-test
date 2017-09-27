33f5a60a63acfcdfb0302c867d474bab718beb42
Sergey Linev <S.Linev@gsi.de>
2017-09-27 19:36:08 +0200

    Introduce testdiff script to compare SVG files

---
+++ testdiff.sh
@@ -0,0 +1,12 @@
+# script dedicated to show difference between file in repository and current file on disk
+# problem with direct diff that files has no line breaks and very difficult to analyze
+
+git show master:$1 > file0.svg
+
+xmllint --format file0.svg > file1.svg
+
+xmllint --format $1 > file2.svg
+
+colordiff file1.svg file2.svg
+
+rm -f file0.svg file1.svg file2.svg
