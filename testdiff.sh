e4606bcc9af3e09dd9cabec939e032429051c826
Sergey Linev <S.Linev@gsi.de>
2020-10-30 12:17:34 +0100

    fix noimg testing

--- testdiff.sh
+++ testdiff.sh
@@ -3,15 +3,18 @@
 
 git show master:$1 > file0.svg
 
-xmllint --format file0.svg > file1.svg
+xmllint --format file0.svg > file1_.svg
 
-xmllint --format $1 > file2.svg
+xmllint --format $1 > file2_.svg
 
 if [[ "$2" == "noimg" ]]; then
-   sed '/<image/d' file1.svg > file1.svg
-   sed '/<image/d' file2.svg > file2.svg
+   sed '/<image/d' file1_.svg > file1.svg
+   sed '/<image/d' file2_.svg > file2.svg
+else
+   mv file1_.svg file1.svg
+   mv file2_.svg file2.svg
 fi
 
 colordiff file1.svg file2.svg
 
-rm -f file0.svg file1.svg file2.svg
+rm -f file0.svg file1.svg file2.svg file1_.svg file2_.svg
